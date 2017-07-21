e30fb24ab7d6c34cd9a7e472181e0e6ed9f12c17
pat <puxt>
2017-07-20 21:24:27 -0500

    More account methods for bittrex

--- ArbitrageBot/APIs/Bittrex/BittrexRequest.cs
+++ ArbitrageBot/APIs/Bittrex/BittrexRequest.cs
@@ -96,7 +96,55 @@ namespace ArbitrageBot.APIs.Bittrex
         public dynamic GetMarketSummary(string market)
         {
             Url += "/getmarketsummary?market=" + market;
-            return GetData(Url); 
+            return GetData(Url);
+        }
+
+        /// <summary>
+        /// Used to retrieve the latest trades that have occured for a specific market
+        /// "success" : true,
+           //	"message" : "",
+        //	"result" : [{
+        //			"Id" : 319435,
+        //			"TimeStamp" : "2014-07-09T03:21:20.08",
+        //			"Quantity" : 0.30802438,
+        //			"Price" : 0.01263400,
+        //			"Total" : 0.00389158,
+        //			"FillType" : "FILL",
+        //			"OrderType" : "BUY"
+        //        }, {
+        //			"Id" : 319433,
+        //			"TimeStamp" : "2014-07-09T03:21:20.08",
+        //			"Quantity" : 0.31820814,
+        //			"Price" : 0.01262800,
+        //			"Total" : 0.00401833,
+        //			"FillType" : "PARTIAL_FILL",
+        //			"OrderType" : "BUY"
+        //		}, {
+        //			"Id" : 319379,
+        //			"TimeStamp" : "2014-07-09T02:58:48.127",
+        //			"Quantity" : 49.64643541,
+        //			"Price" : 0.01263200,
+        //			"Total" : 0.62713377,
+        //			"FillType" : "FILL",
+        //			"OrderType" : "SELL"
+        //		}, {
+        //			"Id" : 319378,
+        //			"TimeStamp" : "2014-07-09T02:58:46.27",
+        //			"Quantity" : 0.35356459,
+        //			"Price" : 0.01263200,
+        //			"Total" : 0.00446622,
+        //			"FillType" : "PARTIAL_FILL",
+        //			"OrderType" : "BUY"
+        //		}
+        //	]
+        //}
+        /// </summary>
+        /// <param name="market"></param>
+        /// <returns></returns>
+        public dynamic GetMarketHistory(string market)
+        {
+            Url += "/getmarkethistory?market=" + market;
+            return GetData(Url);
         }
 
         /// <summary>
@@ -172,6 +220,114 @@ namespace ArbitrageBot.APIs.Bittrex
             Url += string.Format("/selllimit?apikey={0}&market={1}&quantity={2}&rate={3}");
             return GetData(Url);
         }
+
+        /// <summary>
+        /// Used to retrieve all balances from your account
+        ///  {
+        //	"success" : true,
+        //	"message" : "",
+        //	"result" : [{
+        //			"Currency" : "DOGE",
+        //			"Balance" : 0.00000000,
+        //			"Available" : 0.00000000,
+        //			"Pending" : 0.00000000,
+        //			"CryptoAddress" : "DLxcEt3AatMyr2NTatzjsfHNoB9NT62HiF",
+        //			"Requested" : false,
+        //			"Uuid" : null
+
+        //        }, {
+        //			"Currency" : "BTC",
+        //			"Balance" : 14.21549076,
+        //			"Available" : 14.21549076,
+        //			"Pending" : 0.00000000,
+        //			"CryptoAddress" : "1Mrcdr6715hjda34pdXuLqXcju6qgwHA31",
+        //			"Requested" : false,
+        //			"Uuid" : null
+        //		}
+        //	]
+        //}
+        /// </summary>
+        /// <returns></returns>
+        public dynamic GetBalances()
+        {
+            Url += string.Format("/getbalances?apikey={0}" , KeyLoader.BittrexKeys);
+            return GetData(Url);
+        }
+
+        /// <summary>
+        /// Used to retrieve the balance from your account for a specific currency
+        /// {
+        //	"success" : true,
+        //	"message" : "",
+        //	"result" : {
+        //		"Currency" : "BTC",
+        //		"Balance" : 4.21549076,
+        //		"Available" : 4.21549076,
+        //		"Pending" : 0.00000000,
+        //		"CryptoAddress" : "1MacMr6715hjds342dXuLqXcju6fgwHA31",
+        //		"Requested" : false,
+        //		"Uuid" : null
+        //	}
+        //}
+        /// 
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <returns></returns>
+        public dynamic GetBalance(string currency)
+            {
+                Url += string.Format("/getbalance?apikey={0}&currency={1}", KeyLoader.BittrexKeys.Item1, currency);
+                return GetData(Url);
+        }
+
+        /// <summary>
+        /// Used to retrieve or generate an address for a specific currency. If one does not exist, the call will fail and return ADDRESS_GENERATING until one is available
+        /// {
+	    // "success" : true,
+	    // "message" : "",
+	    // "result" : {
+		//  "Currency" : "VTC",
+	    //  "Address" : "Vy5SKeKGXUHKS2WVpJ76HYuKAu3URastUo"
+	    // }
+        //}
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <returns></returns>
+        public dynamic GetDepositAddress(string currency)
+            {
+                Url += string.Format("/getdepositaddress?apikey={0}&currency={1}", KeyLoader.BittrexKeys.Item1, currency);
+                return GetData(Url);
+            }
+
+        /// <summary>
+        /// Used to withdraw funds from your account. note: please account for txfee.
+        /// {
+        //	"success" : true,
+        //	"message" : "",
+        //	"result" : {
+        //			"uuid" : "68b5a16c-92de-11e3-ba3b-425861b86ab6"
+        //	}
+        //}
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <param name="quantity"></param>
+        /// <param name="address"></param>
+        /// <param name="paymentid"></param>
+        /// <returns></returns>
+        public dynamic Withdraw(string currency, string quantity, string address, string paymentid = "")
+            {
+                Url += string.Format("/withdraw?apikey={0}&currency={1}&quantity={2}&address={3}", KeyLoader.BittrexKeys.Item1, currency, quantity, address);
+                if (paymentid != "")
+                {
+                    Url += "&paymentid=" + paymentid;
+                }
+                return GetData(Url);
+            }
+
+            public dynamic GetOrder(string uuid)
+            {
+                Url += string.Format("/getorder?apikey={0}&uuid={1}", KeyLoader.BittrexKeys.Item1, uuid);
+                return GetData(Url);
+            }
 
         /// <summary>
         /// takes the string version of a uri
